96a11d97134bac60ac0a04f5b3cf9da9bd4e8210
Akash Dahad <akash@bhyve.io>
2025-02-16 19:19:59 +0530

    Gemini Connected

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { lazy, Suspense, useEffect, useState } from "react";
 const ChatBot = lazy(() => import("react-chatbotify"));
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  NEW_REGISTRATION_PROMPT,
+  INSURANCE_QUERY_PROMPT,
+} from "../constants/prompts";
 
 const Chatbot = () => {
   const [flowType, setFlowType] = useState<string | null>(null);
   const [form, setForm] = useState<any>({});
+  let hasEnded = false;
   const formStyle = {
     marginTop: 10,
     marginLeft: 20,
@@ -12,6 +18,29 @@ const Chatbot = () => {
     padding: 10,
     borderRadius: 5,
     maxWidth: 300,
+  };
+
+  const key = process.env.NEXT_PUBLIC_AI_KEY || "";
+  const genAI = new GoogleGenerativeAI(key);
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+  const ai_conversation_response = async (params: any) => {
+    // await params.injectMessage("Thank you for using our service.");
+    let prompt = "";
+    if (flowType === "New Registration") {
+      prompt = NEW_REGISTRATION_PROMPT;
+    } else {
+      prompt = INSURANCE_QUERY_PROMPT;
+    }
+    if (params.userInput === "NA") {
+      hasEnded = true;
+      return "Goodbye";
+    }
+    const result = await model.generateContent(prompt);
+    if (result?.response?.candidates?.length)
+      return result?.response?.candidates[0].content.parts[0].text;
+    hasEnded = true;
+    return "Sorry, Something when wrong, Please try again";
   };
 
   const flow = {
@@ -30,15 +59,23 @@ const Chatbot = () => {
         flowType === "New Registration"
           ? "What is your name?"
           : "What is your query?",
-      path: "end",
+      path: "loop",
+    },
+    loop: {
+      message: async (params: any) => {
+        const response = await ai_conversation_response(params);
+        console.log("response", response);
+        return response;
+      },
+      path: () => {
+        if (hasEnded) {
+          return "start";
+        }
+        return "loop";
+      },
     },
     end: {
       message: "Start Again ?",
-      // component: (
-      //   <div style={formStyle}>
-      //     <p>Name: {form?.name}</p>
-      //   </div>
-      // ),
       options: ["New Registration", "Insurance Query", "Nothing More"],
       chatDisabled: true,
       path: "ai_flow",
@@ -47,9 +84,19 @@ const Chatbot = () => {
   return (
     <ChatBot
       settings={{
-        general: { embedded: true },
+        general: { embedded: true, showFooter: false },
         header: {
-          title: "Insurance Bot",
+          title: (
+            <div
+              style={{
+                margin: 0,
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              Insurance Bot
+            </div>
+          ),
           buttons: [],
         },
         footer: {
---
+++ constants/prompts.ts
@@ -0,0 +1,4 @@
+export const NEW_REGISTRATION_PROMPT =
+  "Please enter your name and email address to register";
+
+export const INSURANCE_QUERY_PROMPT = "Please enter your query";
